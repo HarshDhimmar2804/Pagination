41b23c5e0b5d44bc55233d273cd3f03f890b92fe
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-07-04 23:44:36 +0530

    add pagination

---
+++ pagination/src/components/Pagination.jsx
@@ -0,0 +1,51 @@
+const Pagination = ({ pageNo, setPageNo }) => {
+  const prevThreeNoArr = Array.from(
+    { length: 3 },
+    (_, index) => pageNo - 1 - index
+  )
+    .filter((value) => value > 0)
+    .reverse();
+
+  const nextFourNoArr = Array.from(
+    { length: 4 },
+    (_, index) => pageNo + index
+  ).filter((value) => value > 0);
+
+  const paginationArr = [...prevThreeNoArr, ...nextFourNoArr];
+
+  const handleNext = () => {
+    setPageNo(pageNo + 1);
+  };
+
+  const handlePrev = () => {
+    setPageNo(pageNo - 1);
+  };
+  return (
+    <div className="pagination-container">
+      {pageNo > 1 ? (
+        <div onClick={handlePrev} className="page-btn">
+          {"<"}
+        </div>
+      ) : (
+        ""
+      )}
+      {paginationArr.map((value, index) => {
+        return (
+          <div
+            onClick={() => setPageNo(value)}
+            className={value == pageNo ? `page-btn active` : `page-btn`}
+            key={index}
+          >
+            {value}
+          </div>
+        );
+      })}
+
+      <div onClick={handleNext} className="page-btn">
+        {">"}
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
